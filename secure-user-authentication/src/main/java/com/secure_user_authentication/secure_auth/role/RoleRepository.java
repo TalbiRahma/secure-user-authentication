64410d1b8dd5ff3301927e79e6c602dbe80c608a
talbi rahma <talbirahma73@gmail.com>
2024-07-19 11:10:48 +0100

    feat: add RoleRepository interface
    - create RoleRepository interface extending JpaRepository
    - add method to find Role by name

---
+++ secure-user-authentication/src/main/java/com/secure_user_authentication/secure_auth/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.secure_user_authentication.secure_auth.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String role);
+}
