715f33189f2050516dc742ebf753232b84e4997f
talbi rahma <talbirahma73@gmail.com>
2024-07-19 10:53:09 +0100

    feat: add User entity
    - create User class with id, name, email fields
    - add JPA annotations for entity mapping
    - include basic getters and setters

--- secure-user-authentication/src/main/java/com/secure_user_authentication/secure_auth/SecureUserAuthenticationApplication.java
+++ secure-user-authentication/src/main/java/com/secure_user_authentication/secure_auth/SecureUserAuthenticationApplication.java
@@ -2,8 +2,10 @@ package com.secure_user_authentication.secure_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SecureUserAuthenticationApplication {
 
 	public static void main(String[] args) {
