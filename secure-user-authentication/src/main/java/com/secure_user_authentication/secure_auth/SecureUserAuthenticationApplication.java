dfaac98b24e58129765252ec30aef6454b76b78e
talbi rahma <talbirahma73@gmail.com>
2024-07-19 10:40:26 +0100

    feat: initial project setup
    - update application.yml
    - add application-dev.yml
    - modify pom.xml
    - add docker-compose.yml

---
+++ secure-user-authentication/src/main/java/com/secure_user_authentication/secure_auth/SecureUserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.secure_user_authentication.secure_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureUserAuthenticationApplication.class, args);
+	}
+
+}
---
+++ secure-user-authentication/src/test/java/com/secure_user_authentication/secure_auth/SecureUserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure_user_authentication.secure_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureUserAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
