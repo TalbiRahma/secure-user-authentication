c148219fee03b4742805d7867310af37e828a1d9
talbi rahma <talbirahma73@gmail.com>
2024-07-22 17:46:37 +0100

    feat: add JwtFilter for JWT authentication
    - create JwtFilter class extending OncePerRequestFilter
    - inject JwtService
    - implement doFilterInternal method to handle JWT extraction and validation
    - bypass filter for authentication endpoints

--- secure-user-authentication/src/main/java/com/secure_user_authentication/secure_auth/security/JwtFilter.java
+++ secure-user-authentication/src/main/java/com/secure_user_authentication/secure_auth/security/JwtFilter.java
@@ -1,8 +1,39 @@
 package com.secure_user_authentication.secure_auth.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Service
-public class JwtFilter {
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if(request.getServletPath().containes("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
 }
 
